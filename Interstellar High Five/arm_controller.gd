979dde559b38f8606fcaa9d5a906d7128a40b24a
Ricky Guerin <Guerin.Ricky@gmail.com>
2020-05-09 21:16:22 -0400

    add player input

--- Interstellar High Five/arm_controller.gd
+++ Interstellar High Five/arm_controller.gd
@@ -1,10 +1,7 @@
 extends Node
 
 var leftArm
-var leftHand
-
 var rightArm
-var rightHand
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,8 +11,22 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 # warning-ignore:unused_argument
 func _process(delta):
-	leftArm.rotate_object_local(Vector3(0.0, 0.0, 1.0), deg2rad(-2) * delta)
-	leftArm.global_translate(Vector3(0.05, 0.0, 0.0) * delta)
-	
-	rightArm.rotate_object_local(Vector3(0.0, 0.0, 1.0), deg2rad(-2) * delta)
-	rightArm.global_translate(Vector3(-0.05, 0.0, 0.0) * delta)
+	# Move left arm
+	if Input.is_action_pressed("left_arm_move") \
+			and rad2deg(leftArm.rotation.z) > 120:
+		leftArm.rotate_object_local(Vector3(0.0, 0.0, 1.0), deg2rad(-2) * delta)
+		leftArm.global_translate(Vector3(0.05, 0.0, 0.0) * delta)
+
+	elif rad2deg(leftArm.rotation.z) < 145:
+		leftArm.rotate_object_local(Vector3(0.0, 0.0, 1.0), deg2rad(2) * delta)
+		leftArm.global_translate(Vector3(-0.05, 0.0, 0.0) * delta)
+
+	# Move right arm
+	if Input.is_action_pressed("right_arm_move") \
+			and rad2deg(rightArm.rotation.z) > 120:
+		rightArm.rotate_object_local(Vector3(0.0, 0.0, 1.0), deg2rad(-2) * delta)
+		rightArm.global_translate(Vector3(-0.05, 0.0, 0.0) * delta)
+
+	elif rad2deg(rightArm.rotation.z) < 145:
+		rightArm.rotate_object_local(Vector3(0.0, 0.0, 1.0), deg2rad(2) * delta)
+		rightArm.global_translate(Vector3(0.05, 0.0, 0.0) * delta)
