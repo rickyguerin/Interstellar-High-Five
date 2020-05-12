baa3c3bb32689a5f8ea72e065197efe872f719ee
Ricky Guerin <Guerin.Ricky@gmail.com>
2020-05-12 13:00:15 -0400

    fixed timer label formatting

--- Interstellar High Five/game_timer.gd
+++ Interstellar High Five/game_timer.gd
@@ -8,7 +8,9 @@ func _ready():
 
 func _process(delta):
 	timer += delta
-	
-	var format_string = "%02d:%04.2f"
-	
-	set_text(format_string % [(timer / 60), timer])
+
+	var hours = str(int(timer / 3600)).pad_zeros(2)
+	var minutes = str(int(timer / 60)).pad_zeros(2)
+	var seconds = ("%.2f" % fmod(timer, 60.0)).pad_zeros(2)
+
+	set_text(hours + ":" + minutes + ":" + seconds)
