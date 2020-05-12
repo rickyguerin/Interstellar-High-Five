1c65d1666ded4348e13a4e62a49238fdfd77a2d2
Ricky Guerin <Guerin.Ricky@gmail.com>
2020-05-12 13:00:54 -0400

    simplified process by creating vars for common values

--- Interstellar High Five/arm_controller.gd
+++ Interstellar High Five/arm_controller.gd
@@ -11,22 +11,26 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 # warning-ignore:unused_argument
 func _process(delta):
-	# Move left arm
-	if Input.is_action_pressed("left_arm_move") \
-			and rad2deg(leftArm.rotation.z) > 120:
-		leftArm.rotate_object_local(Vector3(0.0, 0.0, 1.0), deg2rad(-2) * delta)
-		leftArm.global_translate(Vector3(0.05, 0.0, 0.0) * delta)
+	var leftRot = rad2deg(leftArm.rotation.z)
+	var rightRot = rad2deg(rightArm.rotation.z)
+	
+	var rotVec = Vector3(0.0, 0.0, 1.0)
+	var transVec = Vector3(0.05, 0.0, 0.0)
 
-	elif rad2deg(leftArm.rotation.z) < 145:
-		leftArm.rotate_object_local(Vector3(0.0, 0.0, 1.0), deg2rad(2) * delta)
-		leftArm.global_translate(Vector3(-0.05, 0.0, 0.0) * delta)
+	# Move left arm
+	if Input.is_action_pressed("left_arm_move") and leftRot > 120:
+		leftArm.rotate_object_local(rotVec, deg2rad(-2) * delta)
+		leftArm.global_translate(transVec * delta)
+
+	elif leftRot < 145:
+		leftArm.rotate_object_local(rotVec, deg2rad(2) * delta)
+		leftArm.global_translate(transVec * -delta)
 
 	# Move right arm
-	if Input.is_action_pressed("right_arm_move") \
-			and rad2deg(rightArm.rotation.z) > 120:
-		rightArm.rotate_object_local(Vector3(0.0, 0.0, 1.0), deg2rad(-2) * delta)
-		rightArm.global_translate(Vector3(-0.05, 0.0, 0.0) * delta)
+	if Input.is_action_pressed("right_arm_move") and rightRot > 120:
+		rightArm.rotate_object_local(rotVec, deg2rad(-2) * delta)
+		rightArm.global_translate(transVec * -delta)
 
-	elif rad2deg(rightArm.rotation.z) < 145:
-		rightArm.rotate_object_local(Vector3(0.0, 0.0, 1.0), deg2rad(2) * delta)
-		rightArm.global_translate(Vector3(0.05, 0.0, 0.0) * delta)
+	elif rightRot < 145:
+		rightArm.rotate_object_local(rotVec, deg2rad(2) * delta)
+		rightArm.global_translate(transVec * delta)
