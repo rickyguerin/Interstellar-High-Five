8a8ee91743758c295f41cee9face7c3d558de24a
Ricky Guerin <guerin.ricky@gmail.com>
2020-05-13 16:31:40 -0400

    make rot vars private global

--- Interstellar High Five/arm_controller.gd
+++ Interstellar High Five/arm_controller.gd
@@ -9,6 +9,9 @@ var left_arm
 var right_arm
 var game_over_flag
 
+var _left_rot
+var _right_rot
+
 func _ready():
 	left_arm = get_parent().get_node("LeftArm")
 	right_arm = get_parent().get_node("RightArm")
@@ -18,28 +21,31 @@ func _process(delta):
 	if game_over_flag:
 		return
 
-	var left_rot = rad2deg(left_arm.rotation.z)
-	var right_rot = rad2deg(right_arm.rotation.z)
+	_left_rot = rad2deg(left_arm.rotation.z)
+	_right_rot = rad2deg(right_arm.rotation.z)
 
-	if left_rot <= 120.05 and right_rot <= 120.05:
+	if _left_rot <= 120.05 and _right_rot <= 120.05:
 		game_over_flag = true
 		emit_signal("game_ended")
 		return
+		
+	_hold_mode(delta)
 
+func _hold_mode(delta):
 	# Move left arm
-	if Input.is_action_pressed("left_arm_move") and left_rot > 120:
+	if Input.is_action_pressed("left_arm_move") and _left_rot > 120:
 		left_arm.rotate_object_local(ROT_VEC, deg2rad(-2) * delta)
 		left_arm.global_translate(TRANS_VEC * delta)
 
-	elif left_rot < 145:
+	elif _left_rot < 145:
 		left_arm.rotate_object_local(ROT_VEC, deg2rad(2) * delta)
 		left_arm.global_translate(TRANS_VEC * -delta)
 
 	# Move right arm
-	if Input.is_action_pressed("right_arm_move") and right_rot > 120:
+	if Input.is_action_pressed("right_arm_move") and _right_rot > 120:
 		right_arm.rotate_object_local(ROT_VEC, deg2rad(-2) * delta)
 		right_arm.global_translate(TRANS_VEC * -delta)
 
-	elif right_rot < 145:
+	elif _right_rot < 145:
 		right_arm.rotate_object_local(ROT_VEC, deg2rad(2) * delta)
 		right_arm.global_translate(TRANS_VEC * delta)
