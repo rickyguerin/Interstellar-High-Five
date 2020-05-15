623b220e5de46fb1f7fd79bfc5577df1b2139ac9
Ricky Guerin <Guerin.Ricky@gmail.com>
2020-05-15 16:46:04 -0400

    remove unnecessary private var

--- Interstellar High Five/mode_label.gd
+++ Interstellar High Five/mode_label.gd
@@ -2,11 +2,9 @@ extends Label
 
 export var arm_name: String
 
-var _mode_controller
-
 func _ready():
-	_mode_controller = get_parent().get_node("Mode Controller")
-	_mode_controller.connect("mode_changed", self, "_update_label")
+	var mode_controller = get_parent().get_node("Mode Controller")
+	mode_controller.connect("mode_changed", self, "_update_label")
 
 func _update_label(updated_arm: String, mode: String):
 	if updated_arm == arm_name:
