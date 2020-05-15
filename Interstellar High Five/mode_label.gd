a618b660fe8624a9d5354a57070c16956f274006
Ricky Guerin <Guerin.Ricky@gmail.com>
2020-05-15 17:38:04 -0400

    change update label to use ints

--- Interstellar High Five/mode_label.gd
+++ Interstellar High Five/mode_label.gd
@@ -8,6 +8,10 @@ func _ready():
 	_mode_controller = get_parent().get_node("Mode Controller")
 	_mode_controller.connect("mode_changed", self, "_update_label")
 
-func _update_label(updated_arm: String, mode: String):
-	if updated_arm == arm_name:
-		text = updated_arm + " Arm Mode:\n" + mode
+func _update_label(updated_arm: int, mode: int):
+	
+	var updated_arm_name = _mode_controller.arm_name(updated_arm)
+	var mode_name = _mode_controller.mode_name(mode)
+	
+	if updated_arm_name == arm_name:
+		text = arm_name + " Arm Mode:\n" + mode_name
