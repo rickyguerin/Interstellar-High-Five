bd7bac84c0f7d4bc50fd632bcbf636f6c4190822
Ricky Guerin <Guerin.Ricky@gmail.com>
2020-05-16 14:40:50 -0400

    generalize menu script

--- Interstellar High Five/menu.gd
+++ Interstellar High Five/menu.gd
@@ -1,8 +1,16 @@
 extends MarginContainer
 
+export var menu_id:int
+
 func _input(event):
-	# If F or Z is pressed, start the game
-	if Input.is_action_pressed("move_arm_0") \
-			and Input.is_action_pressed("move_arm_1"):
-		# warning-ignore:return_value_discarded
-		get_tree().change_scene("res://game.tscn")
+	# F goes back
+	if event.is_action_pressed("move_arm_0"):
+		if menu_id >= 1:
+			get_tree().change_scene("res://menu_" + str(menu_id - 1) + ".tscn")
+	
+	# J continues to next menu or game
+	if event.is_action_pressed("move_arm_1"):
+		if menu_id < 2:
+			get_tree().change_scene("res://menu_" + str(menu_id + 1) + ".tscn")
+		else:
+			get_tree().change_scene("res://game.tscn")
