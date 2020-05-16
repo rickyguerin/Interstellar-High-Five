c9b41581ea650e3976cf6a9a54108eb92b6c9a47
Ricky Guerin <Guerin.Ricky@gmail.com>
2020-05-16 11:50:09 -0400

    require simultaneous F/J to start game

--- Interstellar High Five/menu.gd
+++ Interstellar High Five/menu.gd
@@ -2,7 +2,7 @@ extends MarginContainer
 
 func _input(event):
 	# If F or Z is pressed, start the game
-	if event.is_action_pressed("move_arm_0") \
-			or event.is_action_pressed("move_arm_1"):
+	if Input.is_action_pressed("move_arm_0") \
+			and Input.is_action_pressed("move_arm_1"):
 		# warning-ignore:return_value_discarded
 		get_tree().change_scene("res://game.tscn")
